4b471306081e4393fdbe41ba1bf3ad049f9406c8
xsun89 <sunxin89@gmail.com>
2014-04-10 17:08:43 -0700

    add

--- BankServer/DAL/BankService.cpp
+++ BankServer/DAL/BankService.cpp
@@ -41,7 +41,53 @@ int BankService::UserLogin(const string& user, const string& pass)
 // ¿ª»§
 int BankService::OpenAccount(Account& account)
 {
-	
+	MysqlDB db;
+	Server& server = Singleton<Server>::Instance();
+
+	try
+	{
+		db.Open(server.GetDbServerIp().c_str(),
+			server.GetDbUser().c_str(),
+			server.GetDbPass().c_str(),
+			server.GetDbName().c_str(),
+			server.GetDbServerPort());
+
+		db.StartTransaction();
+		stringstream ss;
+		ss << "insert into bank_account valuses(null, '" <<
+			account.name <<"', '"<<
+			account.pass<<"', '"<<
+			account.id<<"', "<<
+			setprecision(2) << setiosflags(ios::fixed) << account.balance<<", now(), "<<
+			0<<");";
+		unsigned long long ret = db.ExecSQL(ss.str().c_str());
+		ss.clear();
+		account.account_id = static_cast<int>(db.GetInsertId());
+		ss<<"insert into trans values(null, "<<
+			account.account_id<<", null, "<<
+			1<<", "<<
+			setprecision(2)<<setiosflags(ios::fixed)<<account.balance<<", "<<
+			setprecision(2)<<setiosflags(ios::fixed)<<account.balance<<
+			",  now());";
+
+		ret = db.ExecSQL(ss.str().c_str());
+		db.Commit();
+
+		ss.clear();
+		ss.str("");
+		ss<<"select open_date from bank_account where account_id='"<<
+			account.account_id<<"';";
+		MysqlRecordset rs = db.QuerySQL(ss.str().c_str());
+		account.op_date = rs.GetItem(0, "open_date");
+
+	}
+	catch (Exception& e)
+	{
+		LOG_INFO<<e.what();
+		db.Rollback();
+		return -1;
+	}
+
 	return 0;
 }
 
